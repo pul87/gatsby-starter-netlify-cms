ba2cf6780a1f3e1e91f616733b569e34383b4d81
Paolo Basso <paolo.basso87@gmail.com>
2019-12-21 09:44:26 +0100

    Rimozione prodotti e contatti

--- src/components/InstaRoll.js
+++ src/components/InstaRoll.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link, graphql, StaticQuery } from 'gatsby'
-import PreviewCompatibleImage from './PreviewCompatibleImage'
+import { graphql, StaticQuery } from 'gatsby'
 import Img from 'gatsby-image'
 
 class InstaRoll extends React.Component {
